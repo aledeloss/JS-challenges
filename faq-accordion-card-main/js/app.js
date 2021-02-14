7a64dafe988bf562301b084e6f0927e447cdf42d
Ale DeLos <aledeloss@gmail.com>
2021-02-14 09:10:11 -0300

    FAC Accordion mobile v1

---
+++ faq-accordion-card-main/js/app.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.question').forEach(openQuestion);
+
+function openQuestion(item) {
+    item.addEventListener('click', () => {
+        item.nextElementSibling.classList.toggle("showedAnswer")
+        item.children[1].classList.toggle("openedArrow")
+        item.classList.toggle("activeQuestion")
+    })
+}
+
