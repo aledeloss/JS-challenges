e2353cd379bde733e31d1ef5f223568d1e38b791
Ale DeLos <aledeloss@gmail.com>
2021-02-21 09:04:30 -0300

    Estructura y funcionalidad to do app

--- todo-app-main/js/app.js
+++ todo-app-main/js/app.js
@@ -1,66 +1,75 @@
 //Todo list inicial para pruebas
 let registeredTodos = [
-    {
-        "description": "Comer tostadas",
-        "completed": false
-    },
-    {
-        "description": "Dormir",
-        "completed": true
-    }
+  {
+    description: "Comer tostadas",
+    completed: false,
+    enabled: true
+  },
+  {
+    description: "Dormir",
+    completed: true,
+    enabled: false
+  },
+  {
+    description: "Leer",
+    completed: true,
+    enabled: true
+  }
 ];
 
-let filter = 'all';
+let filter = "all";
 
 //To Do list rendering
 //TODO!: Agregar solamente las tareas nuevas a la lista en lugar de borrar todo y volver a agregarlas.
-let allTodos = document.querySelector(".toDos-container");
+let allTodos = document.querySelector(".todos-container");
 
 const filterTodos = () => {
-    switch(filter) {
-        case "complete":
-            filteredList = registeredTodos.filter(todo => todo.completed == true);
-            break;
-        case "active":
-            filteredList = registeredTodos.filter(todo => todo.completed == false);
-            break;
-        case "all":
-        default:
-            filteredList = registeredTodos;
-    }
-    console.log(filteredList)
-    return filteredList;
-}
+  let enabledList = registeredTodos.filter((todo) => todo.enabled == true)
+  switch (filter) {
+    case "complete":
+      filteredList = enabledList.filter((todo) => todo.completed == true);
+      break;
+    case "active":
+      filteredList = enabledList.filter((todo) => todo.completed == false);
+      break;
+    case "all":
+    default:
+      filteredList = enabledList;
+  }
+  return filteredList;
+};
 filterTodos();
 
 const renderTodos = () => {
-    allTodos.innerHTML = "";
-    filterTodos(filter);
-    filteredList.map(
-        (todo) =>
-        (allTodos.innerHTML += `
+  allTodos.innerHTML = "";
+  filterTodos(filter);
+  filteredList.map(
+    (todo) =>
+      (allTodos.innerHTML += `
                 <div class="todo">
-                    <div class="check-container">
-                        <img class="check-icon ${todo.completed == true ? 'check-completed' : ''}" src="images/icon-check.svg" alt="check icon">
+                    <div class="check-container ${
+                      todo.completed == true ? "check-completed" : ""
+                    }">
+                        <img class="check-icon" src="images/icon-check.svg" alt="check icon">
                     </div>
-                    <span class="todo-description ${todo.completed == true ? 'description-completed' : ''}">
+                    <span class="todo-description ${
+                      todo.completed == true ? "description-completed" : ""
+                    }">
                         ${todo.description}
                     </span>
                     <img class="delete-icon" src="images/icon-cross.svg" alt="">
                 </div>
             `)
-    );
-    addCompleteTodo();
-    addDeleteTodo();
-    addFilters()
-    console.log(registeredTodos);
+  );
+  addCompleteTodo();
+  addDeleteTodo();
+  addFilters();
 };
 
 // First Page Load
 window.addEventListener("load", () => {
   renderTodos();
 });
-
 
 // Add a new Todo
 let newTodoInput = document.querySelector("#newTodo");
@@ -69,9 +78,10 @@ const addTodo = newTodo.addEventListener("keypress", function () {
     event.preventDefault();
     console.log(`Registered todo: ${newTodoInput.value}`);
     const newTodo = {
-        "description" : newTodoInput.value,
-        "completed" : false
-    }
+      description: newTodoInput.value,
+      completed: false,
+      enabled: true
+    };
     registeredTodos.push(newTodo);
     console.log(registeredTodos);
     renderTodos();
@@ -80,45 +90,47 @@ const addTodo = newTodo.addEventListener("keypress", function () {
 
 // Complete todo
 const addCompleteTodo = () => {
-    document.querySelectorAll(".check-container").forEach( item => {
-        item.addEventListener("click", event => {
-            item.firstElementChild.classList.toggle("check-completed")
-            item.nextElementSibling.classList.toggle("description-completed")
-            for (i=0; i<registeredTodos.length; i++) {
-                registeredTodos[i].description === item.nextElementSibling.textContent ?
-                registeredTodos[i].completed = !registeredTodos[i].completed : "";
-                console.log(registeredTodos)
-            }
-        })
-    })
-}
+  document.querySelectorAll(".check-container").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      item.classList.toggle("check-completed");
+      item.nextElementSibling.classList.toggle("description-completed");
+      for (i = 0; i < registeredTodos.length; i++) {
+        registeredTodos[i].description == item.nextElementSibling.innerText
+          ? (registeredTodos[i].completed = !registeredTodos[i].completed)
+          : "";
+        console.log(registeredTodos);
+      }
+    });
+  });
+};
 
 // Delete todo
 const addDeleteTodo = () => {
-    document.querySelectorAll(".delete-icon").forEach( item => {
-        item.addEventListener("click", event => {
-            for (i=0; i<registeredTodos.length; i++) {
-                registeredTodos[i].description === item.previousElementSibling.textContent ?
-                registeredTodos.splice(i, 1) : "";
-                console.log(registeredTodos)
-            }
-            renderTodos();
-        })
-    })
-}
+  document.querySelectorAll(".delete-icon").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      for (i = 0; i < registeredTodos.length; i++) {
+        registeredTodos[i].description == item.previousElementSibling.innerText
+          ? registeredTodos[i].enabled = false 
+          : "";
+      }
+      console.log(registeredTodos)
+      renderTodos();
+    });
+  });
+};
 
 // Filters
 const addFilters = () => {
-    document.querySelector(".all").addEventListener("click", () => {
-        filter = "all";
-        renderTodos();
-    })
-    document.querySelector(".active").addEventListener("click", () => {
-        filter = "active";
-        renderTodos();
-    })
-    document.querySelector(".completed").addEventListener("click", () => {
-        filter = "complete";
-        renderTodos();
-    })
-}
+  document.querySelector(".all").addEventListener("click", () => {
+    filter = "all";
+    renderTodos();
+  });
+  document.querySelector(".active").addEventListener("click", () => {
+    filter = "active";
+    renderTodos();
+  });
+  document.querySelector(".completed").addEventListener("click", () => {
+    filter = "complete";
+    renderTodos();
+  });
+};
